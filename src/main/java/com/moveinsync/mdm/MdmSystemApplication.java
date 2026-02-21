033ff73c01a2bf1e351b57c158b96b22b65c83f4
Sai Prashanth <saiprashanth751@gmail.com>
2026-02-21 20:46:53 +0530

    initial-commit

---
+++ src/main/java/com/moveinsync/mdm/MdmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.moveinsync.mdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moveinsync/mdm/MdmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moveinsync.mdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
