63645c0cf18e1a6e6f3d029d9dc085d92b6ed02f
Sai Prashanth <saiprashanth751@gmail.com>
2026-02-21 22:51:11 +0530

    feat:added cache facilities

---
+++ src/main/java/com/moveinsync/mdm/config/CacheConfig.java
@@ -0,0 +1,40 @@
+package com.moveinsync.mdm.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+/**
+ * Redis cache configuration with custom TTLs per cache name.
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withCacheConfiguration("dashboard",
+                        defaultConfig.entryTtl(Duration.ofMinutes(5)))
+                .withCacheConfiguration("versions",
+                        defaultConfig.entryTtl(Duration.ofMinutes(30)))
+                .withCacheConfiguration("compatibility",
+                        defaultConfig.entryTtl(Duration.ofMinutes(15)))
+                .build();
+    }
+}
